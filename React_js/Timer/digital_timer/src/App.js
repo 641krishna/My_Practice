a5c74f11d7ad184a2f498cd0b884e12796c75e77
Shrkrishna Aluri <shrikrishnaaluri641@gmail.com>
2023-03-15 17:05:49 +0530

    make it better

--- React_js/Timer/digital_timer/src/App.js
+++ React_js/Timer/digital_timer/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Digital from './Components/Digital';
 import Usememo from './Components/Usememo';
+import Stopwatch from './Components/Stopwatch';
 
 function App() {
   return (
     <div className="App">
       <Digital />
+     ------------------------------------------
       <Usememo />
+     --------------------------------------------
+      <Stopwatch/>
     </div>
   );
 }
---
+++ React_js/Timer/digital_timer/src/Components/Stopwatch.jsx
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from 'react'
+
+const Stopwatch = () => {
+    const [time, setTime] = useState(null);
+    const [now, setNow] = useState(null);
+
+    const Ref = useRef(null);
+    const handleStart = () => {
+        setTime(Date.now());
+        setNow(Date.now());
+        Ref.current = setInterval(() => {
+            setTime(Date.now());
+        }, 10)
+    }
+
+    const handleStop = () => {
+        clearInterval(Ref.current)
+    };
+
+    const timePassed = (time-now)/1000;
+    return (
+        <div>
+            <h4>StopWatch</h4>
+            <h3>{timePassed }</h3>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
+        </div>
+    )
+}
+
+export default Stopwatch
